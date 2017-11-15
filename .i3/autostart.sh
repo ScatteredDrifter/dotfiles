0e33c1e238435710cb6fa1985371fdebb4aa8fda
Hund <hund@imap.cc>
2017-11-15 23:31:46 +0100

    Err.. updates.

--- .i3/autostart.sh
+++ .i3/autostart.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
 # mouse settings
-xset m 0/0 &
-
-# Keyboard layout
-setxkbmap se &
-
-# Disable NumLock
-numlockx &
+xset m 0/0
 
 # Configure monitors
-xrandr --dpi 96 --output DVI-I-1 --primary --output DVI-D-0 --left-of DVI-I-1 --output HMDI-0 --right-of DVI-I-1 &
-
-# URxvt daemon
-urxvtd -q -f -o &
-
-# Desktop notifications
-dunst &
+xrandr --dpi 96 --output DVI-I-1 --primary --output DVI-D-0 --left-of DVI-I-1 --output HDMI-0 --right-of DVI-I-1
 
 # Hide mouse
 unclutter &
 
+# Sound
+pulseaudio &
+
 # Names day
-sh $HOME/.bin/namnsdag &
+sh $HOME/Scripts/bin/namnsdag
 
-# Twitch notifications
+# Daylight
+sh $HOME/Scripts/bin/daylight
+
+# Notifications
+dunst &
 twitchnotifier -c ebupof &
+mailnag &
 
-# Restore wallpaper
+# Wallpaper
 sh ~/.fehbg &
+#xsetroot -solid '#001E26'
 
 # Set Ranger as default file manager
-xdg-mime default ranger.desktop inode/directory &
+xdg-mime default ranger.desktop inode/directory
+
+# Reload URxvt
+xrdb -merge ~/.Xresources
